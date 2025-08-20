632160d901c6ade222028f152700b4f20abc0351
Ru Chern Chong <hello@ruchern.dev>
2025-08-20 11:35:38 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>Fed Markets Monitor</h1>
+      </header>
+      <main>
+        <p>Federal markets monitoring dashboard</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
